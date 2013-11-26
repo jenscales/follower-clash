666961c12176b840831012accbb3f1a1ba6fc7cb
Ben White <ben.white@utexas.edu>
2013-11-26 15:04:02 -0600

    Add foreman gem, create .env file, add .env file to .gitignore, associate .env keys to follower method

--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -4,6 +4,21 @@ require 'twitter'
 
 module UserComparer
 	class User
+		attr_reader :username
+		def initialize(username)
+			@username = username
+		end
+
+		def followers
+			client = Twitter::REST::Client.new do |config|
+    			config.consumer_key       = ENV['CONSUMER_KEY']
+    			config.consumer_secret    = ENV['CONSUMER_SECRET']
+    			config.access_token        = ENV['OAUTH_TOKEN']
+    			config.access_token_secret = ENV['OAUTH_TOKEN_SECRET']
+    		end
+
+    		client.user(@username)followers_count
+		end
 
 	end
 
